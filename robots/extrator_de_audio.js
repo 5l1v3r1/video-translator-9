3852affa3cfef3dd54e416e26b57e1c2d37829ce
UltraGabriel77 <ghsilha@gmail.com>
2020-09-07 21:09:23 +0000

    criar pasta

--- robots/extrator_de_audio.js
+++ robots/extrator_de_audio.js
@@ -1,10 +1,11 @@
 const extractAudio = require('ffmpeg-extract-audio')
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs')
 
 async function robot() {
     ffmpeg.setFfmpegPath(ffmpegPath);
-
+    fs.mkdirSync('./audio')
     await extractAudio({
     input: './videos_baixados/myvideo.mp4',
     output: './audio/audio.mp4'
