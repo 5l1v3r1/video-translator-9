e6481667ae513ec40d831a21da3f91d6cf0e616f
UltraGabriel77 <ghsilha@gmail.com>
2020-09-07 20:29:16 +0000

    Added Async

--- robots/extrator_de_audio.js
+++ robots/extrator_de_audio.js
@@ -1,6 +1,14 @@
 const extractAudio = require('ffmpeg-extract-audio')
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
 
-await extractAudio({
-  input: 'videos_baixados/myvideo.mp4',
-  output: 'audio/audio.mp4'
-})
+async function robot() {
+    ffmpeg.setFfmpegPath(ffmpegPath);
+
+    await extractAudio({
+    input: './videos_baixados/myvideo.mp4',
+    output: './audio/audio.mp4'
+    }).then(console.log("Audio extraido")).catch()
+}
+
+module.exports = robot
